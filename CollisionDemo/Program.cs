50f23312f742431c1d57a3ac3de7a6fdf9c14f25
Allan Anderson <allana@shaw.ca>
2018-08-10 09:35:54 -0600

    Added CollisionDemo source code

---
+++ CollisionDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CollisionDemo
+{
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Collision())
+                game.Run();
+        }
+    }
+}
